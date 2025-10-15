041a53b77611b42555f631332300e719d23197a2
ba34413 <>
2025-10-15 10:53:43 +0200

    draggable now a class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
-import sys
-from parts import snap_slot
+from parts import snap_slot, draggable
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -10,8 +9,9 @@ dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-rect = pygame.Rect(300, 200, 150, 100)
-snap_rect = snap_slot(screen, 295, 450, 160, 110)
+rect = draggable(screen, 300, 200, 150, 100, "blue")
+snap_rect1 = snap_slot(screen, 295, 450, 160, 110)
+snap_rect2 = snap_slot(screen, 100, 450, 160, 110)
 dragging = False
 
 
@@ -21,34 +21,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if rect.collidepoint(pygame.mouse.get_pos()):
-                dragging = True
-                mouse_x, mouse_y = event.pos
-                offset_x = rect.x - mouse_x
-                offset_y = rect.y - mouse_y
-
-
-        elif event.type == pygame.MOUSEBUTTONUP:
-            dragging = False
-            snap_rect.snap(rect)
-
-        elif event.type == pygame.MOUSEMOTION:
-            if dragging:
-                mouse_x, mouse_y = event.pos
-                rect.x = mouse_x + offset_x
-                rect.y = mouse_y + offset_y
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        print(f"{mouse_x}, {mouse_y}")
+        rect.drag(event)
 
     screen.fill("white")
 
-
-    pygame.draw.rect(screen, "aqua", snap_rect)
-    pygame.draw.rect(screen, "blue", rect)
-
-
-
+    snap_rect1.draw()
+    snap_rect2.draw()
+    rect.draw()
 
     # Renders the game
     pygame.display.flip()
--- parts.py
+++ parts.py
@@ -2,6 +2,8 @@ import pygame
 
 
 class snap_slot():
+    _snapables = {}
+
     def __init__(self, screen, left, top, width, height, color="aqua"):
         self.left = left
         self.top = top
@@ -10,6 +12,7 @@ class snap_slot():
         self.color = color
         self.screen = screen
         self.rect = pygame.Rect(left, top, width, height)
+        snap_slot._snapables[len(snap_slot._snapables) + 1] = self
 
     def draw(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
@@ -19,3 +22,39 @@ class snap_slot():
             draggable.x = self.rect.x + int(self.rect.width / 2) - int(draggable.width / 2)
             draggable.y = self.rect.y + int(self.rect.height / 2) - int(draggable.height / 2)
 
+
+class draggable():
+    def __init__(self, screen, left, top, width=160, height=110, color="aqua"):
+        self.offset_y = None
+        self.offset_x = None
+        self.left = left
+        self.top = top
+        self.width = width
+        self.height = height
+        self.color = color
+        self.screen = screen
+        self.dragging = False
+        self.rect = pygame.Rect(left, top, width, height)
+
+    def draw(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
+
+    def drag(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(pygame.mouse.get_pos()):
+                self.dragging = True
+                mouse_x, mouse_y = event.pos
+                self.offset_x = self.rect.x - mouse_x
+                self.offset_y = self.rect.y - mouse_y
+
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            self.dragging = False
+            for snappable in snap_slot._snapables.values():
+                snappable.snap(self.rect)
+
+        elif event.type == pygame.MOUSEMOTION:
+            if self.dragging:
+                mouse_x, mouse_y = event.pos
+                self.rect.x = mouse_x + self.offset_x
+                self.rect.y = mouse_y + self.offset_y
